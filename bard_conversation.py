73a62360179e4145814a0dc43f96257d670b2108
Nishant <nishantchauhan949@hotmail.com>
2023-05-29 01:39:46 +0530

    Commit:
    1) bard_api_async.py -> BardAPI's async implementation after inheriting from it and using httpx library instead of request library.
    2) bard_conversation.py -> this file is using bard_api_async.BardAsync class now to get answers from Bard.
    3) ai_telegram_bot_async.py -> no major change here.
    4) requirements.txt -> some libraries were upgraded.
    
    On branch upgrade_packages
    	modified:   ai_telegram_bot_async.py
    	new file:   bard_api_async.py
    	modified:   bard_conversation.py
    	modified:   requirements.txt

--- bard_conversation.py
+++ bard_conversation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+import asyncio
 
 from requests import Session
-from bardapi.core import Bard
 
+from bard_api_async import BardAsync
 from settings import BARD_API_KEY, logger
 
 session = Session()
@@ -15,16 +16,16 @@ session.headers = {
     "Referer": "https://bard.google.com/",
 }
 session.cookies.set("__Secure-1PSID", BARD_API_KEY)
-bard = Bard(session=session, timeout=30, token=BARD_API_KEY)
+bard = BardAsync(timeout=30, token=BARD_API_KEY)
 
 
-def get_response_from_bard(input_text, func_name='') -> str:
+async def get_response_from_bard(input_text, func_name='') -> str:
     """
     The get_response_from_bard function takes in a string of text and returns the response from the BARD API.
         Args:
             func_name:
             input_text (str): The user's question to be sent to the BARD API.
-
+    get_bard_object
     Args:
 
     Returns:
@@ -36,15 +37,15 @@ def get_response_from_bard(input_text, func_name='') -> str:
     func_name = func_name + '.get_response_from_bard()' if func_name else 'get_response_from_bard()'
     prepend = f'{func_name} :: '
     try:
-        input_text += '\nPlease keep it under 4096 characters.'
+        # input_text += '\nPlease refrain from using any images.'
         # Send an API request and get a response.
-        response = bard.get_answer(input_text)
-        # logger.info(f'{prepend} type(response) :: {type(response)}')
-        # logger.info(f'response :: {response}')
+        response = await bard.get_answer(input_text)
+        print(f"{response['content']}")
+        print(f"{response['status_code']}")
         return response['content']
     except Exception as e:
         logger.error(f'{prepend} Some unexpected error occurred: {e}')
 
 
 if __name__ == '__main__':
-    get_response_from_bard('What is google?')
+    asyncio.run(get_response_from_bard('What is Meta?'))
